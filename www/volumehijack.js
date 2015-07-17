8439fc2c1f852611ef9b2cf6e2cf3602d8d6b373
Sandro Salles <sandro.salles@ogilvy.com>
2015-07-17 01:16:57 -0300

    first attempt

---
+++ www/volumehijack.js
@@ -0,0 +1,58 @@
+// (c) 2015 Sandro Salles
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* global cordova, module */
+"use strict";
+
+var stringToArrayBuffer = function(str) {
+    var ret = new Uint8Array(str.length);
+    for (var i = 0; i < str.length; i++) {
+        ret[i] = str.charCodeAt(i);
+    }
+    // TODO would it be better to return Uint8Array?
+    return ret.buffer;
+};
+
+var base64ToArrayBuffer = function(b64) {
+    return stringToArrayBuffer(atob(b64));
+};
+
+function massageMessageNativeToJs(message) {
+    if (message.CDVType == 'ArrayBuffer') {
+        message = base64ToArrayBuffer(message.data);
+    }
+    return message;
+}
+
+// Cordova 3.6 doesn't unwrap ArrayBuffers in nested data structures
+// https://github.com/apache/cordova-js/blob/94291706945c42fd47fa632ed30f5eb811080e95/src/ios/exec.js#L107-L122
+function convertToNativeJS(object) {
+    Object.keys(object).forEach(function (key) {
+        var value = object[key];
+        object[key] = massageMessageNativeToJs(value);
+        if (typeof(value) === 'object') {
+            convertToNativeJS(value);
+        }
+    });
+}
+
+module.exports = {
+
+    listen: function (success, failure) {
+        var successWrapper = function(volumeHandle) {
+            success(volumeHandle);
+        };
+        cordova.exec(successWrapper, failure, 'VolumeHijack', 'listen', []);
+    }
+};
